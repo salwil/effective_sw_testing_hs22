597f487f534c9fdbbe90b4de33c96d2f42c4a289
Salome Wildermuth <salome.wildermuth@gmail.com>
2022-09-24 18:20:57 +0200

    funny word task

---
+++ labAssignment1/funny_word/src/main/java/zest/FunnyWord.java
@@ -0,0 +1,21 @@
+package zest;
+
+/**
+ * Solution inspired by: https://www.geeksforgeeks.org/funny-string/
+ */
+class FunnyWord {
+    boolean checkFunny(String word)
+    {
+        int i = 1;
+        int j = word.length() - 2;
+        word = word.toLowerCase();
+        while (i <= j) {
+            if ((Math.abs(word.charAt(i) - word.charAt(i - 1))) !=
+                   Math.abs((word.charAt(j) - word.charAt(j + 1))))
+                return false;
+            i++;
+            j--;
+        }
+        return true;
+    }
+}
---
+++ labAssignment1/funny_word/src/test/java/zest/FunnyWordTest.java
@@ -0,0 +1,20 @@
+package zest;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FunnyWordTest {
+
+    private FunnyWord funnyWord;
+
+    @BeforeEach
+    void setup() {
+        funnyWord = new FunnyWord();
+    }
+
+    @Test
+    void isFunnyWord() {
+    }
+}
