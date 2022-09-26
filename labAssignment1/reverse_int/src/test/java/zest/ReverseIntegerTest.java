5e88c795e7ad0b2ea091184622a968d578d97e98
Salome Wildermuth <salome.wildermuth@gmail.com>
2022-09-26 09:01:03 +0200

    tests for reverse integer completed (100% Andy assessment)

--- labAssignment1/reverse_int/src/test/java/zest/ReverseIntegerTest.java
+++ labAssignment1/reverse_int/src/test/java/zest/ReverseIntegerTest.java
@@ -13,55 +13,58 @@ class ReverseIntegerTest {
 
     private ReverseInteger revInt = new ReverseInteger();
 
-    @Test
-    void reverseIntOneDigit() {
-        assertEquals(revInt.reverse(0),0);
-        assertEquals(revInt.reverse(1),1);
-    }
-    @Test
-    void reverseIntTwoDigits() {
-        assertEquals(revInt.reverse(21),12);
-    }
-
-    @Test
-    void reverseIntWithZeroes() {
-        assertEquals(revInt.reverse(210),12);
-    }
-
-    public static Stream<Arguments> maxIntegers() {
+    // Test on and off points.
+    public static Stream<Arguments> positiveIntegers() {
         return Stream.of(
-                Arguments.of(0, 2147483647),
-                Arguments.of(214748364, 463847412),
+                // corner case derived with structural testing (kills mutant of line 12):
+                Arguments.of(2147483641, 1463847412),
                 Arguments.of(214748374, 473847412),
-                //Arguments.of(463847412, 2147483647 / 10),
-                //Arguments.of(563847412, 2147483647 / 10 + 1),
-                Arguments.of(0, 2147483647 + 1),
-                Arguments.of(0, 2147447417),
+                // highest 32-bit integer:
+                Arguments.of(0, 2147483647),
+                // highest possible value that can be reversed:
                 Arguments.of(2147447412, 2147447412)
         );
     }
 
-    @ParameterizedTest
-    @MethodSource("maxIntegers")
-    void reverseIntMax(int expected, int input){
-        assertEquals(expected, revInt.reverse(input));
-    }
-
-    public static Stream<Arguments> minIntegers() {
+    // Test on and off points.
+    public static Stream<Arguments> negativeIntegers() {
         return Stream.of(
-                Arguments.of(-214748364, -463847412),
-                Arguments.of(-214748365, -563847412),
-                Arguments.of(-214748366, -663847412),
+                // corner case derived with structural testing (kills mutant of line 13):
+                Arguments.of(-2147483641, -1463847412),
+                // lowest 32-bit integer:
                 Arguments.of(0, -2147483648),
-                //Arguments.of(0, -2147483648 - 1),
-                Arguments.of(0, -2147483647),
+                //Arguments.of(0, -2147483647),
+                // lowest possible value that can be reversed:
                 Arguments.of(-2147447412, -2147447412)
         );
     }
 
+    @Test
+    void reverseIntOneDigit() {
+        assertEquals(revInt.reverse(0), 0);
+        assertEquals(revInt.reverse(1), 1);
+    }
+
+    @Test
+    void reverseIntTwoDigits() {
+        assertEquals(revInt.reverse(21), 12);
+    }
+
+    @Test
+    void reverseIntWithZeroes() {
+        assertEquals(revInt.reverse(210), 12);
+        assertEquals(revInt.reverse(201), 102);
+    }
+
     @ParameterizedTest
-    @MethodSource("minIntegers")
-    void reverseIntMin(int expected, int input){
+    @MethodSource("positiveIntegers")
+    void reverseIntPositive(int expected, int input) {
+        assertEquals(expected, revInt.reverse(input));
+    }
+
+    @ParameterizedTest
+    @MethodSource("negativeIntegers")
+    void reverseIntNegative(int expected, int input) {
         assertEquals(expected, revInt.reverse(input));
     }
 }
