3a2b4023e0a2ee6ce8097370f25031f1112b9fbc
Salome Wildermuth <salome.wildermuth@gmail.com>
2022-09-26 07:07:27 +0200

    tests for longest common prefix

--- labAssignment1/longest_common_prefix/src/test/java/zest/LongestCommonPrefixTest.java
+++ labAssignment1/longest_common_prefix/src/test/java/zest/LongestCommonPrefixTest.java
@@ -1,10 +1,64 @@
 package zest;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class LongestCommonPrefixTest {
+    LongestCommonPrefix lcp = new LongestCommonPrefix();
 
-    @Test
-    void longestCommonPrefix() {
+    private static Stream<Arguments> commonPrefix() {
+        return Stream.of(
+                Arguments.of(new String[]{"abc", "acd"}, "a"),
+                Arguments.of(new String[]{"abcd", "abc", "ab"}, "ab"),
+                Arguments.of(new String[]{"b", "b"}, "b"),
+                // The three test cases below are enough for the Andy assessment,
+                // but we should add some more variants and corner test cases:
+                Arguments.of(new String[]{"abc", "abd"}, "ab"),
+                Arguments.of(new String[]{"b"}, "b"),
+                Arguments.of(new String[]{"bc", "b"}, "b"),
+                Arguments.of(new String[]{"abc", "abc", "abc"}, "abc")
+        );
+    }
+
+    private static Stream<Arguments> noCommonPrefix() {
+        return Stream.of(
+                Arguments.of(new String[]{"abc", "xyz"}, ""),
+                Arguments.of(new String[]{"bcd", "a"}, ""),
+                Arguments.of(new String[]{"abc", ""}, ""),
+                // The three test cases below are enough for the Andy assessment,
+                // but I prefer to add one more:
+                Arguments.of(new String[]{"a", "bcd"}, "")
+        );
+    }
+
+    private static Stream<Arguments> emptyInputs() {
+        return Stream.of(
+                Arguments.of(new String[]{""}, ""),
+                Arguments.of(null, ""),
+                Arguments.of(new String[]{}, "")
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("commonPrefix")
+    void longestCommonPrefix(String[] strs, String expected) {
+        assertEquals(lcp.longestCommonPrefix(strs), expected);
+    }
+
+    @ParameterizedTest
+    @MethodSource("noCommonPrefix")
+    void noCommonPrefix(String[] strs, String expected) {
+        assertEquals(lcp.longestCommonPrefix(strs), expected);
+    }
+
+    @ParameterizedTest
+    @MethodSource("emptyInputs")
+    void noCommonPrefixEmptyInputs(String[] strs, String expected) {
+        assertEquals(lcp.longestCommonPrefix(strs), expected);
     }
 }
